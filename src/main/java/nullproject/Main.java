98c4ac435523ed4897ada3500d29d329cb0c4f96
Greatcan <bilasebastyan@gmail.com>
2018-10-26 22:13:18 +0300

    Hack your mind - Added first scene

---
+++ src/main/java/nullproject/Main.java
@@ -0,0 +1,9 @@
+package nullproject;
+
+public class Main {
+
+    public static void main(String[] args) {
+        InLectureHall inLectureHall = new InLectureHall();
+        inLectureHall.start();
+    }
+}
